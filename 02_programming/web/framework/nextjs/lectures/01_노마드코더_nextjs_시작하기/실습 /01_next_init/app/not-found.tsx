6ef7f28633fb3869c247c6b6c1119f704441a8ca
didxodnr0769-bom <54312928+didxodnr0769-bom@users.noreply.github.com>
2025-03-21 15:39:18 +0900

    fix : not found page gnb 제거

--- 02_programming/web/framework/nextjs/lectures/01_노마드코더_nextjs_시작하기/실습 /01_next_init/app/not-found.tsx
+++ 02_programming/web/framework/nextjs/lectures/01_노마드코더_nextjs_시작하기/실습 /01_next_init/app/not-found.tsx
@@ -1,10 +1,3 @@
-import Gnb from "../components/gnb";
-
 export default function NotFoundPage() {
-  return (
-    <div>
-      <Gnb />
-      Not Found! 경로를 확인하세요
-    </div>
-  );
+  return <div>Not Found! 경로를 확인하세요</div>;
 }
